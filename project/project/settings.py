d92ab6ac0f36e469ab910adf2f44e7f1f95a7877
Gopi <sistlagopi3@gmail.com>
2024-06-14 14:58:09 +0530

    sixth commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'localhost:1521/XE',
-        'USER': 'bank',
-        'PASSWORD': 'bank',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
